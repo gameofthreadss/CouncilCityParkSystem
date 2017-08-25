6191b16b299408fd2e991aa9fdae8a980bf366b6
Pratikk <patelpratik512@outlook.com>
2017-08-25 15:11:23 +1000

    Added Commets to object creation of adhockTicket and its interface class
    Added Commets to object creation of adhockTicket and its interface class

--- src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -1,15 +1,15 @@
 package bcccp.tickets.adhoc;
 
 import java.util.List;
-import bcccp.tickets.adhoc.IAdhocTicket;
-import bcccp.tickets.adhoc.AdhocTicket;
+import bcccp.tickets.adhoc.IAdhocTicket;//access iAdhocTicket Class methods
+import bcccp.tickets.adhoc.AdhocTicket;//access AdhocTicket Class data
 
 public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 
 	private IAdhocTicketFactory factory;
 	private int currentTicketNo;
-        private IAdhocTicket iAdhocTicket;
-        private AdhocTicket adhocTicket;
+        private IAdhocTicket iAdhocTicket; // object creation to access iAdhocTicket Class methods
+        private AdhocTicket adhocTicket; //object creation to access AdhocTicket Class data
 
 	
 	
