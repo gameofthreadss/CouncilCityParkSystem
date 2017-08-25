ac4c3746740560f2d2dc78164d8eeb7dd2940b96
Pratikk <patelpratik512@outlook.com>
2017-08-25 15:32:13 +1000

    Implemented CreateTicket() for dhocTicketDAO class
    Implemented CreateTicket() for dhocTicketDAO class

--- src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -3,15 +3,18 @@ package bcccp.tickets.adhoc;
 import java.util.List;
 import bcccp.tickets.adhoc.IAdhocTicket;//access iAdhocTicket Class methods
 import bcccp.tickets.adhoc.AdhocTicket;//access AdhocTicket Class data
-
+import bcccp.carpark.Carpark;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.entry.IEntryController;
 public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 
 	private IAdhocTicketFactory factory;
 	private int currentTicketNo;
         private IAdhocTicket iAdhocTicket; // object creation to access iAdhocTicket Class methods
         private AdhocTicket adhocTicket; //object creation to access AdhocTicket Class data
-
-	
+        private Carpark carPark;
+	private EntryUI entryUI;
+        private IEntryController iEntryController;
 	
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
 		//Constructor Implementation
@@ -22,8 +25,17 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	@Override
 	public IAdhocTicket createTicket(String carparkId) {
 		// TODO Auto-generated method stub
-                
-		return null;
+                if(carPark.isFull())
+                      entryUI.display("Full");
+                else
+                {
+                    entryUI.pushButton();
+                    iAdhocTicket.getBarcode();
+                    iAdhocTicket.getTicketNo();
+                    iAdhocTicket.getCarparkId();
+		
+                }
+                return iAdhocTicket;
 	}
 
 
