4d2a06a747822338fc4b0c8d1f3de9b6b28d5be5
oshan2csd <oshan2csd@gmail.com>
2017-08-25 05:34:31 -0700

    end time
    end time will return. method completed

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -49,8 +49,7 @@ public class SeasonTicket implements ISeasonTicket {
 
 	@Override
 	public long getEndValidPeriod() {
-		// TODO Auto-generated method stub
-		return 0;
+            return this.endValidPeriod;
 	}
 
 	@Override
